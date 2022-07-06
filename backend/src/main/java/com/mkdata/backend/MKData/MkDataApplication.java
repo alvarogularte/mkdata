753f2250d81da11306542fc6c77c56d0a49f2650
alvarogularte <alvaro.gularte@gmail.com>
2022-07-06 17:58:02 -0300

    new: create model, service and controller directories

---
+++ backend/src/main/java/com/mkdata/backend/MKData/MkDataApplication.java
@@ -0,0 +1,13 @@
+package com.mkdata.backend.MKData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkDataApplication.class, args);
+	}
+
+}
