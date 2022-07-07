7033b84b9fd664e37a670b41c6bd26ab75c7e5fb
alvarogularte <alvaro.gularte@gmail.com>
2022-07-07 16:23:11 -0300

    new: create criarCliente method - controller

--- backend/src/main/java/com/mkdata/backend/MKData/controller/ClienteController.java
+++ backend/src/main/java/com/mkdata/backend/MKData/controller/ClienteController.java
@@ -1,5 +1,23 @@
 package com.mkdata.backend.MKData.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mkdata.backend.MKData.model.Cliente;
+import com.mkdata.backend.MKData.service.ClienteService;
+
+@RestController
 public class ClienteController {
 
+  @Autowired
+  ClienteService clienteService;
+  
+  @PostMapping("/clientes")
+  public ResponseEntity<Cliente> criarCliente(@RequestBody Cliente cliente) {
+    Cliente clienteCriado = clienteService.criarCliente(cliente);
+    return ResponseEntity.ok(clienteCriado);
+  }
 }
