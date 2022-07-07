d8854c79798f096046c6bc4590c4aa64aa809778
alvarogularte <alvaro.gularte@gmail.com>
2022-07-07 16:25:40 -0300

    new: create LlistarClientes method - controller

--- backend/src/main/java/com/mkdata/backend/MKData/controller/ClienteController.java
+++ backend/src/main/java/com/mkdata/backend/MKData/controller/ClienteController.java
@@ -1,7 +1,10 @@
 package com.mkdata.backend.MKData.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +22,11 @@ public class ClienteController {
   public ResponseEntity<Cliente> criarCliente(@RequestBody Cliente cliente) {
     Cliente clienteCriado = clienteService.criarCliente(cliente);
     return ResponseEntity.ok(clienteCriado);
+  }
+  
+  @GetMapping("/clientes")
+  public ResponseEntity<List<Cliente>> listarClientes() {
+    List<Cliente> clientes = clienteService.listarClientes();
+    return ResponseEntity.ok(clientes);
   }
 }
