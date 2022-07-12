76a4d90a540bf1795f3d2f7d0088fc61e74ca187
alvarogularte <alvaro.gularte@gmail.com>
2022-07-11 23:43:20 -0300

    new: create controller test

---
+++ backend/src/test/java/com/mkdata/backend/MKData/controller/ClienteControllerTest.java
@@ -0,0 +1,42 @@
+package com.mkdata.backend.MKData.controller;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.mkdata.backend.MKData.service.ClienteService;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ClienteControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+  
+  @MockBean
+  private ClienteService clienteService;
+  
+  @Test
+  void retornaListaVaziaQuandoNaoHouverClientes() throws Exception {
+    doReturn(List.of()).when(clienteService).listarClientes();
+
+    final var resposta = mockMvc.perform(get("/clientes"));
+    
+    resposta
+      .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+      .andExpect(status().isOk())
+      .andExpect(content().string(containsString("[]")));
+  }
+}
