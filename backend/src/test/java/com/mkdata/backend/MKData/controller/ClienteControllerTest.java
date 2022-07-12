4e53ae55f53962a06a3d6ce3f908da07d658716f
alvarogularte <alvaro.gularte@gmail.com>
2022-07-12 00:09:53 -0300

    fix: fixing erros

--- backend/src/test/java/com/mkdata/backend/MKData/controller/ClienteControllerTest.java
+++ backend/src/test/java/com/mkdata/backend/MKData/controller/ClienteControllerTest.java
@@ -8,6 +8,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.util.List;
 
+import com.mkdata.backend.MKData.service.ClienteService;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -15,8 +17,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
-import com.mkdata.backend.MKData.service.ClienteService;
 
 @SpringBootTest
 @AutoConfigureMockMvc
